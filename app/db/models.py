aebd8e1e491ed2c76cb858d27867fac77084fde1
reelstaskmaster <ret.andrey@mail.ru>
2026-09-19 12:26:26 +0700

    fix: remove nonexistent message delivery key

--- app/db/models.py
+++ app/db/models.py
@@ -54,7 +54,6 @@ class Message(Base):
     )
 
     id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, default=uuid.uuid4)
-    delivery_key: Mapped[str] = mapped_column(String(64), nullable=False)
     chat_id: Mapped[int] = mapped_column(
         BigInteger, ForeignKey("chats.id", ondelete="CASCADE"), nullable=False
     )
